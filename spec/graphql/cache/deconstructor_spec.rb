890546382758fdda2001dba52c9961dd5a1c5c46
Vince DeVendra <vdevendra@wistia.com>
2019-10-04 12:11:59 -0400

    Allow the deconstructor to return a promise
    The deconstructor continues to return the same values it did previously,
    except now, if the object was a promise, or the object was an array
    containing a promise, the return value will be wrapped in a promise.
    
    This allows for compatibility with graphql-batch, which returns
    promises.

--- spec/graphql/cache/deconstructor_spec.rb
+++ spec/graphql/cache/deconstructor_spec.rb
@@ -19,6 +19,17 @@ module GraphQL
           it 'should return raw.object' do
             expect(subject.perform).to eq raw.object
           end
+
+          context 'when object is a promise that responds to object' do
+            let(:raw) { Promise.resolve(double('Raw', object: 'foo')) }
+            let(:method) { 'promise' }
+
+            it 'should return a promise that resolves to raw.object' do
+              subject.perform.then do |resolved|
+                expect(resolved).to eq raw.object
+              end
+            end
+          end
         end
 
         context 'when method is "array"' do
@@ -39,6 +50,24 @@ module GraphQL
 
             it 'should return an array of the inner objects' do
               expect(subject.perform).to eq ['foo', 'bar']
+            end
+          end
+
+          context 'at least one object is a promise' do
+            let(:raw) do
+              [
+                Promise.resolve(1),
+                Promise.resolve(2),
+                3,
+              ]
+            end
+
+            it 'should return a promise that resolves to an array of the inner objects' do
+              puts subject.perform
+
+              subject.perform.then do |resolved|
+                expect(resolved).to eq [1, 2, 3]
+              end
             end
           end
         end
@@ -71,6 +100,17 @@ module GraphQL
 
           it 'should return a flat array of nodes' do
             expect(subject.perform).to eq [1,2,3]
+          end
+
+          context 'when raw is a promise that resolves to a subclass of Graphql::Relay::BaseConnection' do
+            let(:raw) { Promise.resolve(GraphQL::Relay::BaseConnection.new(nodes, [])) }
+            let(:method) { 'promise' }
+
+            it 'should return a promise that resolves to a flat array of nodes' do
+              subject.perform.then do |resolved|
+                expect(resolved).to eq [1,2,3]
+              end
+            end
           end
         end
       end
